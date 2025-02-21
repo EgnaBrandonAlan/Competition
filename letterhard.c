7454b4c897dc877eeea93de7d3f38a21b8902497
EgnaBrandonAlan <142899741+EgnaBrandonAlan@users.noreply.github.com>
2025-02-21 10:16:04 -0500

    Solution to G). Letter Factory (Harder) in C

---
+++ letterhard.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+    int q, T, a, b, d, k;
+    char *s;
+    scanf("%d", &q);
+    scanf("%s", s);
+
+    for (int i = 0; i < q; i++){
+        scanf("%d", &T);
+        if (T == 0){
+            scanf(" %d %d %d", &a, &b, &d);
+            for (int j = a; j <= b; j++){
+                if (d == 0){
+                    s[j] = (s[j] == 'a') ? 'z' : s[j] - 1;
+                } else{
+                    s[j] = (s[j] == 'z') ? 'a' : s[j] + 1;
+                }
+            }
+        } else if (T == 1){
+            scanf(" %d", &k);
+            printf("%c\n", s[k]);
+        }
+    }
+}
